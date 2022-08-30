06bb8f524f2eea72de53f6e92fdcff2dea4b002d
Jose Kajal <josekajal_reynosa@hotmail.com>
2022-08-29 20:40:31 -0700

    Avance guias

---
+++ LogisticForStudent/Guiasinf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogisticForStudent
+{
+    public class Guiasinf
+    {
+        public string[] Itemlist { get; set; }
+        public Guiasinf()
+        {
+            //  Itemlist = new string[] { "Credenciales", "Constancias Estudios", "Certificados", "Horarios", "Ciclo escolar" };
+
+        }
+    }
+
+}
---
+++ LogisticForStudent/Views/Noticiasxaml.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LogisticForStudent.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Noticiasxaml : ContentPage
+    {
+        public Noticiasxaml()
+        {
+            InitializeComponent();
+            lstguias.ItemsSource = new string[] { "Credenciales", "Constancias Estudios", "Certificados", "Horarios", "Ciclo escolar" };
+
+
+        }
+    }
+}
